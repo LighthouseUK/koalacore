17aba213162b820abb3782907ed6d288c9788c58
Matt Badger <matt@lighthouseuk.net>
2016-05-20 13:01:10 +0100

    Fixed version number for download_url in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Matt Badger',
     author_email='foss@lighthouseuk.net',
     url='https://github.com/LighthouseUK/koalacore',  # use the URL to the github repo
-    download_url='https://github.com/LighthouseUK/koalacore/tarball/0.1',  # I'll explain this in a second
+    download_url='https://github.com/LighthouseUK/koalacore/tarball/0.1.1-alpha',  # I'll explain this in a second
     keywords=['gae', 'lighthouse'],  # arbitrary keywords
     classifiers=[],
 )
