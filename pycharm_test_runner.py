c73a02587c016d0de0383b61d4e219aed4d04d34
Matt Badger <matt+imac@lighthouseuk.net>
2016-11-28 13:07:12 +0000

    Fixed the test runner so we don't need to hardcode the sdk path. Consolidated the test runner files to avoid confusion.

--- pycharm_test_runner.py
+++ pycharm_test_runner.py
@@ -2,7 +2,26 @@
 This file is just a helper for running unittests in PyCharm.
 As the module test files are not in the root of the project the 'vendor' setups fom *.yaml break.
 Subsequently the unittest will fail to load as the dependencies are missing.
+
+In addition, because of some quirks with the app engine SDK loader, we have to fix the system path and use a non-default
+version of webob (webtest will not work without a version > 1.2)
 """
+import os
+import sys
+
+def fix_sys_path():
+    try:
+        from dev_appserver import fix_sys_path, _DIR_PATH
+        fix_sys_path()
+        # must be after fix_sys_path
+        # uses non-default version of webob
+        webob_path = os.path.join(_DIR_PATH, 'lib', 'webob-1.2.3')
+        sys.path = [webob_path] + sys.path
+    except ImportError, e:
+        print 'fix failed: {}'.format(e.message)
+
+fix_sys_path()
+
 import unittest
 from koalacore.tests.test_task import TestTaskHandler
 # from koalacore.tests.test_api import TestResource
@@ -17,9 +36,5 @@ from koalacore.tests.test_task import TestTaskHandler
 __author__ = 'Matt'
 
 
-class TestApex(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+if __name__ == '__main__':
+    unittest.main()
